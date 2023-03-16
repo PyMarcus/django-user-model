ec7c613221fefd0f1228a8d74e8d0abb4c59de00
MarcusV <Marcus-v@outlook.com>
2023-03-16 19:52:45 -0300

    create some settings to admin file user

--- core/admin.py
+++ core/admin.py
@@ -6,6 +6,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
 
     list_display = ('title', '_author')
+    exclude = ['author']  # hidden author
 
     def _author(self, instance) -> str:
         return str(instance.author.get_full_name())
@@ -13,3 +14,7 @@ class PostAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super(PostAdmin, self).get_queryset(request)
         return qs.filter(author=request.user) # search post only the logged user
+
+    def save_model(self, request, obj, form, change): # all
+        obj.author = request.user
+        super().save_model(request, obj, form, change) # allow save post withou the author label
